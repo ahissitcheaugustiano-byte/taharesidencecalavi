62d064a340ea323d11871cc1b2d0ec692accde12
Augustiano-PC <dev@example.com>
2026-04-23 14:20:34 +0200

    fix: update Google Maps embed URL for precise location

--- src/components/LocationSection.tsx
+++ src/components/LocationSection.tsx
@@ -27,7 +27,7 @@ const LocationSection = () => {
             transition={{ duration: 0.6 }}
           >
             <iframe
-              src="https://maps.google.com/maps?q=TAHA%20RESIDENCE,%20Abomey-Calavi,%20Benin&t=&z=16&ie=UTF8&iwloc=&output=embed"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.2415174092523!2d2.342237075738499!3d6.429337493561578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1024a97ebb99ee99%3A0x18043a60ba4cef50!2sTAHA%20RESIDENCE!5e0!3m2!1sfr!2sbj!4v1714000000000!5m2!1sfr!2sbj"
               className="w-full h-full border-0"
               allowFullScreen
               loading="lazy"
