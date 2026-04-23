c903c4b0b6066b91714c12f951b6ff1be2d15110
Augustiano-PC <dev@example.com>
2026-04-23 13:13:56 +0200

    Update precise Google Maps location pin

--- src/components/LocationSection.tsx
+++ src/components/LocationSection.tsx
@@ -27,7 +27,7 @@ const LocationSection = () => {
             transition={{ duration: 0.6 }}
           >
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3965.0!2d2.35!3d6.45!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNsKwMjcnMDAuMCJOIDLCsDIxJzAwLjAiRQ!5e0!3m2!1sfr!2sbj!4v1700000000000"
+              src="https://maps.google.com/maps?q=TAHA%20RESIDENCE,%20Abomey-Calavi,%20Benin&t=&z=16&ie=UTF8&iwloc=&output=embed"
               className="w-full h-full border-0"
               allowFullScreen
               loading="lazy"
